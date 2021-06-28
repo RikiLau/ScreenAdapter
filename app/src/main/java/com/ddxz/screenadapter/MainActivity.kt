a4b42e9ceefbd4bc341cad2d4cadc9635c142396
riki <moshoubu@sina.com>
2021-06-28 16:25:51 +0800

    v1.5

---
+++ app/src/main/java/com/ddxz/screenadapter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ddxz.screenadapter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ScreenUtils.testAa()
+    }
+}
